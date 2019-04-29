67c35c39d66e58dcbd62746650b7fd2f7f081f4a
Vidhi <vidhisshah@gmail.com>
2019-04-29 19:23:36 -0400

    first

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, url_for, render_template
+from book_info_extractor import get_info_book_picks, display_table
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+	tabs = ['Home','Blog','Events','Newsletter','FAQ','About','Contact']
+	return render_template("index.html")
+	# return ("Quills Library Home page")
+
+@app.route('/books')
+def books():
+	filepath = "../static/images/1984'.jpg"
+	book_name = "1984'.jpg"
+	# print (get_info_book_picks())
+	df = display_table()
+	return render_template("books.html", filepath=filepath, book_picks_info=get_info_book_picks(), \
+		tables=[df.to_html(classes='data', header="true", table_id='myTable')])
+
+
+@app.route('/fake')
+def fake():
+	return render_template("fake.html", book_picks_info=get_info_book_picks())
+
+@app.route('/blog')
+def blog():
+	return render_template("blog.html", book_picks_info=get_info_book_picks())
+
+@app.route('/events')
+def events():
+	return render_template('events.html')
+
+# debatable. should come with blog?
+@app.route('/Newsletter')
+def newsletter():
+	return("Sign in to receive bi-monthly Newsletter\nView previous Newsletters here")
+
+@app.route('/faq')			
+def faq():
+	return "FAQ: Membership, timing, late fee, donations, etc"
+
+@app.route('/about')	
+def about():
+	return "Founders. Communal space. Library. Learning environment"
+
+@app.route('/contact')	
+def contact():
+	return render_template('contact.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ book_info_extractor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+# from tqdm import tqdm
+
+
+# In[3]:
+
+
+book_df = pd.read_csv('Quills_and_goodreads_db.csv')
+
+
+# In[4]:
+
+
+def get_info_book_picks():    
+    book_picks = ["1984'","Alchemist", "Wintercraft"]
+    book_picks_df_list = []
+    book_picks_df = pd.DataFrame(columns=book_df.columns)
+    for i,book in enumerate(book_picks):
+        book_picks_df_list.append(book_df.loc[book_df['Title']==book].drop_duplicates(subset='Title'))
+    book_picks_df = book_picks_df.append(book_picks_df_list)    
+    return book_picks_df
+
+
+# In[6]:
+
+
+def create_table():
+    create_df = book_df.drop('Description', axis=1,inplace=False)
+    for i in create_df.index:
+        create_df['Title'].iloc[i] = " ".join(create_df['Title'].iloc[i].split("+")) 
+    create_df.to_csv("book_author_rating.csv")    
+
+def display_table():
+    display_df = pd.read_csv('book_author_rating.csv')
+    return display_df
+
+
+
+
